ef6b395200db736c5af5c04e432822587b6ad0a1
Chieftonn <infante.joshua1994@gmail.com>
2021-03-29 12:54:54 -0700

    Added player_controls.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from settings import *
+from player_controls import *
 
 pygame.init()
 screen = pygame.display.set_mode(
     size=Setting().resolution
 )
 while Setting().is_running:
+    Player.control()
     for event in pygame.event.get():
         if event.type == QUIT:
             quit()
