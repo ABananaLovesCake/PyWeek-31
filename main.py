24eec1966b699f30f886bf6c76c012a56ae6ad14
Chieftonn <infante.joshua1994@gmail.com>
2021-03-29 12:22:45 -0700

    Added settings.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
+from settings import *
 
+pygame.init()
+pygame.display.set_mode(
+    size=Settings().resolution
+)
 
 if __name__ == '__main__':
     pass
---
+++ settings.py
@@ -0,0 +1,5 @@
+
+class Settings(object):
+    def __init__(self):
+        resolution = (1280, 720)
+        self.resolution = resolution
